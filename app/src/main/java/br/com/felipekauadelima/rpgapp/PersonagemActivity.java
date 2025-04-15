7b933b25730157eac901591d416bfacbf6e821ff
Felipe Kauã de Lima <fkauadelima@gmail.com>
2025-04-15 20:10:51 -0300

    Entrega 1

--- app/src/main/java/br/com/felipekauadelima/rpgapp/PersonagemActivity.java
+++ app/src/main/java/br/com/felipekauadelima/rpgapp/PersonagemActivity.java
@@ -2,7 +2,10 @@ package br.com.felipekauadelima.rpgapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class PersonagemActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextClasse;
+    private CheckBox checkBoxNpc;
+    private RadioGroup radioGroupAlinhamento;
+    private Spinner spinnerRaca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,28 @@ public class PersonagemActivity extends AppCompatActivity {
 
         editTextNome = findViewById(R.id.editTextNome);
         editTextClasse = findViewById(R.id.editTextClasse);
+        checkBoxNpc = findViewById(R.id.checkBoxNpc);
+        radioGroupAlinhamento = findViewById(R.id.radioGroupAlinhamento);
+        spinnerRaca = findViewById(R.id.spinnerRaca);
+
+//        popularSpinner();
     }
+
+//    private void popularSpinner(){
+//        ArrayList<String> lista = new ArrayList<>();
+//        lista.add(getString(R.string.humano));
+//        lista.add(getString(R.string.anao));
+//        lista.add(getString(R.string.elfo));
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista);
+//        spinnerRaca.setAdapter(adapter);
+//    }
 
     public void limparCampos(View view){
         editTextNome.setText(null);
         editTextClasse.setText(null);
+        checkBoxNpc.setChecked(false);
+        radioGroupAlinhamento.clearCheck();
+        spinnerRaca.setSelection(0);
         editTextNome.requestFocus();
         Toast.makeText(this, R.string.as_entradas_foram_apagadas, Toast.LENGTH_SHORT).show();
     }
@@ -48,7 +71,37 @@ public class PersonagemActivity extends AppCompatActivity {
 
         classe = classe.trim();
 
-        Toast.makeText(this, getString(R.string.nome_valor)+nome+"\n"+getString(R.string.classe_valor) + classe, Toast.LENGTH_LONG).show();
+        boolean npc = checkBoxNpc.isChecked();
+
+        int radioButtonId = radioGroupAlinhamento.getCheckedRadioButtonId();
+
+        String alinhamento;
+
+        if(radioButtonId == R.id.radioButtonBom){
+            alinhamento = getString(R.string.bom);
+        }else if(radioButtonId == R.id.radioButtonMau) {
+            alinhamento = getString(R.string.mau);
+        }else if(radioButtonId == R.id.radioButtonNeutro){
+            alinhamento = getString(R.string.neutro);
+        }else{
+            Toast.makeText(this, R.string.selecao, Toast.LENGTH_SHORT).show();
+            radioGroupAlinhamento.requestFocus();
+            return;
+        }
+
+        String raca = (String) spinnerRaca.getSelectedItem();
+
+        if(raca == null){
+            Toast.makeText(this, R.string.spinner, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Toast.makeText(this, getString(R.string.nome_valor)+nome+"\n"+
+                                         getString(R.string.classe_valor)+classe+"\n"+
+                                         (npc ? getString(R.string.npc) : getString(R.string.nao_npc))+"\n"+
+                                         getString(R.string.alinhamento_valor)+alinhamento+"\n"+
+                                         getString(R.string.raca_valor)+raca,
+                                         Toast.LENGTH_LONG).show();
 
     }
 }
